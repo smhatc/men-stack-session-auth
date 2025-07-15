92614f51564a768578946125ec1c2762e65de765
Sadeq Talaq <165420981+smhatc@users.noreply.github.com>
2025-07-15 15:35:50 +0300

    Added sign up post request logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,26 @@
 // SETUP
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user.js");
 
 // ROUTES
 router.get("/sign-up", (req, res) => {
         res.render("./auth/sign-up.ejs");
+});
+
+router.post("/sign-up", async (req, res) => {
+        const formData = req.body;
+        const userInDatabase = await User.findOne({ username: formData.username, });
+        if (!userInDatabase) {
+                if (formData.password !== formData.confirmPassword) {
+                        return res.send("The password and confirm password do not match, please try again.");
+                } else {
+                        const newUser = await User.create(formData);
+                        res.send(`Thank you for signing up, ${newUser.username}!`);
+                }
+        } else {
+                return res.send("An account with this username already exists.");
+        }
 });
 
 // EXPORTING ROUTES
