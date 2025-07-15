7db67ce9c4ef32147202672c9884198dd61b23ed
Sadeq Talaq <165420981+smhatc@users.noreply.github.com>
2025-07-15 14:02:22 +0300

    Created directory structure and setup server.js plus index view

---
+++ server.js
@@ -0,0 +1,31 @@
+// SETUP
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT ? process.env.PORT : "3000";
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+
+// DATABASE CONNECTION
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+        console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(methodOverride("_method"));
+
+// ROUTES
+app.get("/", (req, res) => {
+        res.render("index.ejs", {
+                title: "My App"
+        });
+});
+
+// STARTING THE SERVER
+app.listen(port, () => {
+        console.log(`Server is listening on port ${port}.`);
+});
